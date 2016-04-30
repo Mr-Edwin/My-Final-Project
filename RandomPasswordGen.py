cef6b1c45e25b33eb48d86183b21f655d121edeb
Mr-Edwin <ecruz87@mail.valenciacollege.edu>
2016-04-29 22:11:50 -0400

    Code V2
    I added a random letter and symbol chooser

--- RandomPasswordGen.py
+++ RandomPasswordGen.py
@@ -1,34 +1,60 @@
 import random
-
-password = []
-placeholder = 0
-choices = []
-
-
-print('How long would you like your password to be?')
-
-passLength = input()
-
-while len(password) < int(passLength):
-    password = password + [placeHolder]
-    placeHolder = placeHolder + 1
-    
-while len(choices) < 3:
-    print('Hello, what would you like your password to consist of?')
-    print('1.Uppercase' '\n' '2.Lowercase' '\n' '3.Symbols' '\n' '4.Nothing Else')
-
-    choice = input()
-    choices = choices + [choice]
-    if 1 in choices:
-
-upperCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+uppercaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
                    'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
                     'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]
-upperCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+lowercaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
                    'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
                     's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
 symbols = [ '!', '@', '#', '$', '%', '^', '&', '*', '(',
             ')', '-', '=', '_', '+', '[', ']', '{', '}',
             '|', ';', ':', ',', '.', '<', '>', '?', '~']
-def randomUpperCase:
-    random.randint
+password = []
+placeHolder = 0
+choices = []
+options = ['1.Uppercase', '2.Lowercase', '3.Symbols', '4.Nothing Else']
+
+print('How long would you like your password to be?')
+
+passLength = int(input())
+
+while len(password) < int(passLength):
+    password = password + [placeHolder]
+    placeHolder = placeHolder + 1
+
+while len(choices) < 3:
+    print('Hello, what would you like your password to consist of?')
+    print("\n".join(options))
+
+    choice = int(input())
+    choices = choices + [choice]
+    try:
+        if 1 in choices:
+            print('WOO!')
+            del options[0]
+        elif 2 in choices:
+            print('NOO!')
+            del options[1]
+        elif 3 in choices:
+            print('IDK')
+            del options[2]
+        elif 4 in choices:
+            break
+    except IndexError:
+        print('Please enter a value in the list!')
+        continue
+    break
+print('All done!, your password is ' + str(password))
+
+def randomUppercase
+    randomNumber = random.randint(0,26)
+    randomUppercase = uppercaseLetters[randomNumber]
+    global randomUppercase
+
+def randomLowercase
+    randomNumber = random.randint(0,26)
+    randomLowercase = lowercaseLetters[randomNumber]
+    global randomLowercase
+def randomSymbol
+    randomNumber = random.randint(0,26)
+    randomSymbol = Symbols[randomNumber]
+    global randomSymbol
